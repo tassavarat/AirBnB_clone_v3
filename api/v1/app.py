a9837c99bfffb30f8108a6a5289a455913201614
Sofia Cheung <sofiavictoria17@gmail.com>
2019-09-09 23:52:31 +0000

    Adding scripts for task 3

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+The app module
+"""
+
+from models import storage
+from api.v1.views import app_views
+from flask import Flask
+from os import getenv
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def close_storage():
+    """calls storage.close() method"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host=getenv(host=getenv'HBNB_API_HOST', port=getenv('HBNB_API_PORT)
+
