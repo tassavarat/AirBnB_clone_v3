458548f8c88a7c5a117e3e10df2b008a4b68aa03
Sofia Cheung <sofiavictoria17@gmail.com>
2019-09-10 02:10:39 +0000

    Adding new route retriever

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,9 +5,24 @@ index module
 
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models import storage
 
 
 @app_views.route("/status", methods=["GET"])
 def status():
     """Returns a JSON string"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", methods=["GET"])
+def stats_class_count():
+    """Retrieves the number of each object by type"""
+    dic = {
+            'amenities': storage.count('Amenity'),
+            'cities': storage.count('City'),
+            'places': storage.count('Place'),
+            'reviews': storage.count('Review'),
+            'states': storage.count('State'),
+            'users': storage.count('User')
+            }
+    return dic
