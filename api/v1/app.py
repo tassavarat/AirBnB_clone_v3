51b69a06495e3f88c4dcbf1b277ec4b102be7131
Tim Assavarat <tassavarat@gmail.com>
2019-09-10 01:31:04 +0000

    First commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,14 +9,15 @@ from flask import Flask
 from os import getenv
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
-def close_storage():
+def close_storage(exception):
     """calls storage.close() method"""
     storage.close()
 
-if __name__ == "__main__":
-    app.run(host=getenv(host=getenv'HBNB_API_HOST', port=getenv('HBNB_API_PORT)
 
+if __name__ == "__main__":
+    app.run(host=getenv("HBNB_API_HOST"), port=getenv("HBNB_API_PORT"),
+            threaded=True)
