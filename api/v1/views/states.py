bb7cb624d462ade10eb08db315e5b986260cdc45
Sofia Cheung <sofiavictoria17@gmail.com>
2019-09-11 21:24:33 +0000

    Added create and update methods for state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,9 +4,10 @@ states view module
 """
 
 from models import storage
+from models.state import State
 from models.base_model import BaseModel
 from api.v1.views import app_views
-from flask import Flask, jsonify, make_response, request
+from flask import Flask, jsonify, make_response, request, abort
 
 
 @app_views.route("/states", methods=["GET"])
@@ -17,7 +18,7 @@ def get_states():
     for i in dic_t:
         js = i.to_dict()
         li.append(js)
-    return jsonify(li)
+    return jsonify(li), 200
 
 
 @app_views.route("/states/<state_id>", methods=["GET"])
@@ -25,9 +26,9 @@ def get_state_id(state_id):
     """Returns a State object based on: state_id"""
     d = storage.get("State", state_id)
     if d:
-        return jsonify(d.to_dict())
+        return jsonify(d.to_dict()), 200
     else:
-        return make_response(jsonify({"error": "Not found"}), 404)
+        abort(404)
 
 
 @app_views.route("/states/<state_id>", methods=["DELETE"])
@@ -37,16 +38,38 @@ def delete_state_id(state_id):
     if d:
         storage.delete(d)
         storage.save()
-        return jsonify({})
+        return jsonify({}), 200
     else:
-        return make_response(jsonify({"error": "Not found"}), 404)
+        abort(404)
 
 
 @app_views.route("/states", methods=["POST"])
 def create_states():
     """Creates a State object"""
-    print("####")
-    obj = request.get_json()
-    print("output type is", type(request.get_json))
-    print(obj.get("name"))
-    print("####")
+    dic_t = request.get_json()
+    if not dic_t:
+        abort(400, {"Not a JSON"})
+    if 'name' not in dic_t:
+        abort(400, {"Missing name"})
+    new_state = State(**dic_t)
+    storage.new(new_state)
+    storage.save()
+    d = new_state.to_dict()
+    return jsonify(d), 201
+
+
+@app_views.route("/states/<state_id>", methods=["PUT"])
+def update_states(state_id):
+    """Updates a State object"""
+    dic_t = request.get_json()
+    d = storage.get("State", state_id)
+    if not d:
+        abort(404)
+    if not dic_t:
+        abort(400, {"Not a JSON"})
+    if 'name' in dic_t:
+        for k, v in dic_t.items():
+            setattr(d, k, v)
+        storage.save()
+    to_d = d.to_dict()
+    return jsonify(to_d), 200
