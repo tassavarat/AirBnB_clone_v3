a3066c1a50323c279f1a420cbe34f1b194932e89
Sofia Cheung <sofiavictoria17@gmail.com>
2019-09-11 02:25:03 +0000

    Added new route for states_id

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,31 +8,20 @@ from models.base_model import BaseModel
 from api.v1.views import app_views
 from flask import Flask, jsonify
 
-"""
-li = []
-dic = storage.all('State').values()
-for i in dic:
-    for k, v in i.to_dict().items():
-        print(k, v)
-        print("########")
-        print(jsonify({k: v}))
-
-print("#########################")
-li = []
-dic = storage.all('State').values()
-for i in dic:
-    js = i.to_dict()
-    li.append(js)
-print(li)
-"""
-
 
 @app_views.route("/states", methods=["GET"])
-def status():
+def get_states():
     """Returns a JSON string"""
     li = []
-    dic = storage.all('State').values()
-    for i in dic:
+    dic_t = storage.all('State').values()
+    for i in dic_t:
         js = i.to_dict()
         li.append(js)
-    return li
+    return jsonify(li)
+
+
+@app_views.route("/states/<state_id>", methods=["GET"])
+def get_state_id(state_id):
+    '''Returns a State object based on: state_id'''
+    d = storage.get("State", state_id)
+    return jsonify(d.to_dict())
